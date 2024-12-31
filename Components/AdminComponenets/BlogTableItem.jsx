68e534f79a171140bfaf276deb28e692107385df
Rohit Sharma <rohits8731@gmail.com>
2024-12-31 11:12:53 +0530

    initial commit

---
+++ Components/AdminComponenets/BlogTableItem.jsx
@@ -0,0 +1,28 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({authorImg,title,author,date,deleteBlog,mongoId}) => {
+
+    const blogDate = new Date(date);
+
+  return (
+    <tr className=' bg-white border-b'>
+        <th scope='row' className=' items-center gap-3 sm:flex hidden px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+            <Image width={40} height={40} src={authorImg?authorImg:assets.profile_icon}></Image>
+            <p>{author?author:"No Author"}</p>
+        </th>
+        <td className=' px-6 py-4'>
+            {title?title:"no Title"}
+        </td>
+        <td className=' px-6 py-4'>
+            {blogDate.toDateString()}
+        </td>
+        <td onClick={()=>deleteBlog(mongoId)} className=' px-6 py-4 cursor-pointer'>
+            x
+        </td>
+    </tr>
+  )
+}
+
+export default BlogTableItem
